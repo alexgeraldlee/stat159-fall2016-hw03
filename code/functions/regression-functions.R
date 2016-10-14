267f4a082ab418ad6a8b08b2bb4976ac5e125420
Alexander Lee <tofu.da.beast.42@gmail.com>
2016-10-13 23:35:17 -0700

    First few functions of regression-functions.R completed

---
+++ code/functions/regression-functions.R
@@ -0,0 +1,19 @@
+residual_sum_squares <- function(lm_object){
+  return(sum((lm_object$residuals)^2))
+}
+
+total_sum_squares <- function(lm_object){
+  #this function only can take an lm() object for a single regression
+  #since R cannot return multiple values (easily), we can only return one
+  #TSS is the amount of variability in the response variable
+  data_set <- lm_object$model
+  return(sum((data_set[,1] - mean(data_set[,1]))^2))
+}
+
+r_squared <- function(lm_object){
+  return(1 - residual_sum_squares(lm_object)/total_sum_squares(lm_object))
+}
+
+f_statistic <- function(lm_object){
+  
+}
